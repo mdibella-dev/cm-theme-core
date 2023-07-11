3ba916093dd6dc5bf4ff833139a0b821733c8c15
Marco Di Bella <mdb@marcodibella.de>
2023-07-11 11:50:10 +0200

    Remove from plugin

--- includes/shortcodes/index.php
+++ includes/shortcodes/index.php
@@ -6,7 +6,6 @@ defined( 'ABSPATH' ) or exit;
 
 require_once( 'shortcode-event-table.php' );
 require_once( 'shortcode-speaker-grid.php' );
-require_once( 'shortcode-teaser-list.php' );
 require_once( 'shortcode-partner-table.php' );
 require_once( 'shortcode-icon-wall.php' );
 require_once( 'shortcode-exhibition-list.php' );
