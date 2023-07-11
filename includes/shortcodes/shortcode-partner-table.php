f9537024c0688ca085adcac95a9e3f21caa1852d
Marco Di Bella <mdb@marcodibella.de>
2023-07-11 11:28:16 +0200

    Change primary language to EN

--- includes/shortcodes/shortcode-partner-table.php
+++ includes/shortcodes/shortcode-partner-table.php
@@ -90,7 +90,7 @@ function shortcode_partner_table( $atts, $content = null )
                             $cells[ 'partner-logo' ] = sprintf(
                                 '<a href="%1$s" target="_blank" title="%2$s" rel="external">%3$s</a>',
                                 $link,
-                                __( 'Externen Link aufrufen', 'cm-theme-core' ),
+                                __( 'Open external link', 'cm-theme-core' ),
                                 $image
                             );
                         else :
@@ -143,7 +143,7 @@ function shortcode_partner_table( $atts, $content = null )
 
                             if( ! empty( $number ) ) :
                                 $strings[] = sprintf(
-                                    __( 'Stand %1$s', 'cm-theme-core' ),
+                                    __( 'Booth %1$s', 'cm-theme-core' ),
                                     $number
                                 );
                             endif;
