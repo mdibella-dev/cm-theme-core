9e7b13c64b8518e0c2928c382baa07eb85a721af
Marco Di Bella <mdb@marcodibella.de>
2023-07-20 09:21:20 +0200

    Rename function

--- includes/post-types/post-type-speaker.php
+++ includes/post-types/post-type-speaker.php
@@ -151,7 +151,8 @@ add_action( 'pre_get_posts', __NAMESPACE__ . '\post_type_speaker__pre_get_posts'
  * @since 1.0.0
  */
 
-function cptui_register_my_cpts() {
+function post_type_speaker__register()
+{
 
 
     /**
@@ -206,4 +207,4 @@ function cptui_register_my_cpts() {
 
 }
 
-add_action( 'init', __NAMESPACE__ . '\cptui_register_my_cpts' );
+add_action( 'init', __NAMESPACE__ . '\post_type_speaker__register' );
