8bd2580f52fd087922a8bed8465e5461a8382fb5
Marco Di Bella <mdb@marcodibella.de>
2023-07-20 10:28:14 +0200

    Change array notation

--- includes/taxonomies/taxonomy-exhibition-package.php
+++ includes/taxonomies/taxonomy-exhibition-package.php
@@ -23,14 +23,14 @@ defined( 'ABSPATH' ) or exit;
 
 function taxonomy_exhibition_package__manage_edit_columns( $default )
 {
-    $columns = array(
+    $columns = [
         'cb'          => $default['cb'],
         'id'          => 'ID',
         'name'        => $default['name'],
         'description' => $default['description'],
         'slug'        => $default['slug'],
         'count'       => __( 'Count', 'cm-theme-core' ),
-    );
+    ];
     return $columns;
 }
 add_filter( 'manage_edit-exhibition_package_columns', __NAMESPACE__ . '\taxonomy_exhibition_package__manage_edit_columns' );
@@ -45,21 +45,22 @@ add_filter( 'manage_edit-exhibition_package_columns', __NAMESPACE__ . '\taxonomy
 
 function taxonomy_exhibition_package__manage_custom_column( $content, $column_name, $term_id )
 {
-    switch ($column_name) {
+    switch ($column_name)
+    {
         case 'id':
             $content = $term_id;
         break;
 
         case 'count':
-            $posts = get_posts( array(
+            $posts = get_posts( [
                 'post_type'   => 'exhibition_space',
                 'post_status' => 'any',
                 'numberposts' => -1,
-                'tax_query'   => array( array(
+                'tax_query'   => [ [
                     'taxonomy' => 'exhibition_package',
                     'terms'    => $term_id,
-                ) ),
-            ) );
+                ] ],
+            ] );
             $term    = get_term( $term_id, 'exhibition_package' );
             $content = sprintf(
                 '<a href="/wp-admin/edit.php?exhibition_package=%2$s&post_type=exhibition_space" title="%3$s">%1$s</a>',
