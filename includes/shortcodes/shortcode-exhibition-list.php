bf638cc68f73fd19a785db112597890af8262726
Marco Di Bella <mdb@marcodibella.de>
2023-07-20 10:17:52 +0200

    Change array notation

--- includes/shortcodes/shortcode-exhibition-list.php
+++ includes/shortcodes/shortcode-exhibition-list.php
@@ -32,29 +32,29 @@ function shortcode_exhibition_list( $atts, $content = null )
 {
     /** Determine passed parameters. */
 
-    $default_atts = array(
+    $default_atts = [
         'partnership' => '',
-    );
+    ];
     extract( shortcode_atts( $default_atts, $atts ) );
 
 
     /** Retrieve and prepare data. */
 
-    $query = array(
+    $query = [
         'post_type'      => 'partner',
         'post_status'    => 'publish',
         'posts_per_page' => '-1',
         'order'          => 'ASC',
         'orderby'        => 'title',
-    );
+    ];
 
     // Optionally, you can filter by type of partnership.
     if( ! empty( $partnership ) ) :
-        $query[ 'tax_query' ] = array( array(
+        $query['tax_query'] = [ [
             'taxonomy' => 'partnership',
             'field'    => 'term_id',
-            'terms'    => explode(',', $partnership ),
-        ) );
+            'terms'    => explode( ',', $partnership ),
+        ] ];
     endif;
 
     $partners = get_posts( $query );
@@ -72,14 +72,14 @@ function shortcode_exhibition_list( $atts, $content = null )
     ?>
 
     <li class="exhibition-list-element">
-        <a href="<?php echo $data[ 'permalink' ]; ?>">
+        <a href="<?php echo $data['permalink']; ?>">
             <figure>
                 <?php echo get_the_post_thumbnail( $partner->ID, 'full' ); ?>
             </figure>
             <div>
-                <h3><?php echo $data[ 'title' ]; ?></h3>
+                <h3><?php echo $data['title']; ?></h3>
                 <div class="exhibition-list-layout">
-                    <div><?php echo $data[ 'address' ];?></div>
+                    <div><?php echo $data['address'];?></div>
                     <div>
 
                     <?php
@@ -90,8 +90,8 @@ function shortcode_exhibition_list( $atts, $content = null )
                      */
                     $spaces = array();
 
-                    foreach( $data[ 'exhibition-spaces' ] as $space ) :
-                        if( ! empty( $space[ 'location' ] ) and ! empty( $space[ 'signature' ] ) ) :
+                    foreach( $data['exhibition-spaces'] as $space ) :
+                        if( ! empty( $space['location'] ) and ! empty( $space['signature'] ) ) :
                             $spaces[] = $space;
                         endif;
                     endforeach;
