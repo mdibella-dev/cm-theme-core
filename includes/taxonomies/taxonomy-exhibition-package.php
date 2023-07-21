6dc8a0c41b4d7450a3351d93e10b78a5e4127fe2
Marco Di Bella <mdb@marcodibella.de>
2023-07-21 11:58:56 +0200

    Add taxonomy registration function

--- includes/taxonomies/taxonomy-exhibition-package.php
+++ includes/taxonomies/taxonomy-exhibition-package.php
@@ -76,3 +76,49 @@ function taxonomy_exhibition_package__manage_custom_column( $content, $column_na
     return $content;
 }
 add_filter( 'manage_exhibition_package_custom_column', __NAMESPACE__ . '\taxonomy_exhibition_package__manage_custom_column', 10, 3 );
+
+
+
+/**
+ * Registers the exhibition package taxonomy.
+ *
+ * @since 1.0.0
+ */
+
+function taxonomy_exhibition_package__register()
+{
+    $labels = [
+        'name'          => esc_html__( 'Exhibition packages', 'cm-theme-core' ),
+        'singular_name' => esc_html__( 'Exhibition package', 'cm-theme-core' ),
+    ];
+
+
+    $args = [
+        'label'                 => esc_html__( 'Exhibition packages', 'cm-theme-core' ),
+        'labels'                => $labels,
+        'public'                => true,
+        'publicly_queryable'    => true,
+        'hierarchical'          => false,
+        'show_ui'               => true,
+        'show_in_menu'          => true,
+        'show_in_nav_menus'     => false,
+        'query_var'             => true,
+        'rewrite'               => [
+            'slug'       => 'exhibition_package',
+            'with_front' => true,
+        ],
+        'show_admin_column'     => true,
+        'show_in_rest'          => true,
+        'show_tagcloud'         => true,
+        'rest_base'             => 'exhibition_package',
+        'rest_controller_class' => 'WP_REST_Terms_Controller',
+        'rest_namespace'        => 'wp/v2',
+        'show_in_quick_edit'    => false,
+        'sort'                  => false,
+        'show_in_graphql'       => false,
+    ];
+
+    register_taxonomy( 'exhibition_package', ['exhibition_space'], $args );
+}
+
+add_action( 'init', __NAMESPACE__ . '\taxonomy_exhibition_package__register' );
