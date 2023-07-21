d1c0fd388b4597b48c2e6a8821e9f502b5ffb141
Marco Di Bella <mdb@marcodibella.de>
2023-07-21 11:58:51 +0200

    Add taxonomy registration function

--- includes/taxonomies/taxonomy-event.php
+++ includes/taxonomies/taxonomy-event.php
@@ -68,3 +68,51 @@ function taxonomy_event__manage_custom_column( $content, $column_name, $term_id 
     return $content;
 }
 add_filter( 'manage_event_custom_column', __NAMESPACE__ . '\taxonomy_event__manage_custom_column', 10, 3 );
+
+
+
+/**
+ * Registers the event taxonomy.
+ *
+ * @since 1.0.0
+ */
+
+function taxonomy_event__register()
+{
+    $labels = [
+        'name'          => esc_html__( 'Events', 'cm-theme-core' ),
+        'singular_name' => esc_html__( 'Event', 'cm-theme-core' ),
+        'menu_name'     => esc_html__( 'Events', 'cm-theme-core' ),
+        'all_items'     => esc_html__( 'All events', 'cm-theme-core' ),
+    ];
+
+
+    $args = [
+        'label'                 => esc_html__( 'Events', 'cm-theme-core' ),
+        'labels'                => $labels,
+        'public'                => true,
+        'publicly_queryable'    => true,
+        'hierarchical'          => false,
+        'show_ui'               => true,
+        'show_in_menu'          => true,
+        'show_in_nav_menus'     => false,
+        'query_var'             => true,
+        'rewrite'               => [
+            'slug'       => 'event',
+            'with_front' => true,
+        ],
+        'show_admin_column'     => true,
+        'show_in_rest'          => true,
+        'show_tagcloud'         => true,
+        'rest_base'             => 'event',
+        'rest_controller_class' => 'WP_REST_Terms_Controller',
+        'rest_namespace'        => 'wp/v2',
+        'show_in_quick_edit'    => false,
+        'sort'                  => false,
+        'show_in_graphql'       => false,
+    ];
+
+    register_taxonomy( 'event', ['session'], $args );
+}
+
+add_action( 'init', __NAMESPACE__ . '\taxonomy_event__register' );
