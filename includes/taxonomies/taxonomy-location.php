2cd2256aec32f0d851dc62fd35f1cddaddb08465
Marco Di Bella <mdb@marcodibella.de>
2023-07-21 11:58:58 +0200

    Add taxonomy registration function

--- includes/taxonomies/taxonomy-location.php
+++ includes/taxonomies/taxonomy-location.php
@@ -108,3 +108,51 @@ function taxonomy_location__manage_custom_column( $content, $column_name, $term_
     return $content;
 }
 add_filter( 'manage_location_custom_column', __NAMESPACE__ . '\taxonomy_location__manage_custom_column', 10, 3 );
+
+
+
+/**
+ * Registers the location taxonomy.
+ *
+ * @since 1.0.0
+ */
+
+function taxonomy_location__register()
+{
+
+    $labels = [
+        'name'          => esc_html__( 'Locations', 'cm-theme-core' ),
+        'singular_name' => esc_html__( 'Location', 'cm-theme-core' ),
+        'menu_name'     => esc_html__( 'Locations', 'cm-theme-core' ),
+    ];
+
+
+    $args = [
+        'label'                 => esc_html__( 'Locations', 'cm-theme-core' ),
+        'labels'                => $labels,
+        'public'                => true,
+        'publicly_queryable'    => true,
+        'hierarchical'          => false,
+        'show_ui'               => true,
+        'show_in_menu'          => true,
+        'show_in_nav_menus'     => false,
+        'query_var'             => true,
+        'rewrite'               => [
+            'slug'       => 'location',
+            'with_front' => true,
+        ],
+        'show_admin_column'     => true,
+        'show_in_rest'          => true,
+        'show_tagcloud'         => true,
+        'rest_base'             => 'location',
+        'rest_controller_class' => 'WP_REST_Terms_Controller',
+        'rest_namespace'        => 'wp/v2',
+        'show_in_quick_edit'    => false,
+        'sort'                  => false,
+        'show_in_graphql'       => false,
+    ];
+
+    register_taxonomy( 'location', ['session'], $args );
+}
+
+add_action( 'init', __NAMESPACE__ . '\taxonomy_location__register' );
