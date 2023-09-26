5c5d7b31068ab554a2b2bb5da6eaa47e17d9d1c2
Marco Di Bella <mdb@marcodibella.de>
2023-09-26 12:18:03 +0200

    Bump to 2.1.0

--- plugin.php
+++ plugin.php
@@ -9,7 +9,7 @@
  * Requires at least:   6
  * Tested up to:        6.3
  * Requires PHP:        7
- * Version:             2.0.1
+ * Version:             2.1.0
  * Text Domain:         cm-theme-core
  * Domain Path:         /languages
  *
@@ -28,7 +28,7 @@ defined( 'ABSPATH' ) or exit;
 
 /** Variables and definitions */
 
-define( __NAMESPACE__ . '\PLUGIN_VERSION', '2.0.1' );
+define( __NAMESPACE__ . '\PLUGIN_VERSION', '2.1.0' );
 define( __NAMESPACE__ . '\PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( __NAMESPACE__ . '\PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
