9e791e45c63101ea0dfd8ea2302995826cfad2b4
Marco Di Bella <m_dibella@icloud.com>
2025-11-05 07:41:00 +0100

    Install WordPress_Helper composer package

---
+++ vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '0ef6db6ceed30d8eb2f82dedbb78047393d31b9d',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '0ef6db6ceed30d8eb2f82dedbb78047393d31b9d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'mdibella-dev/wordpress-helper' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'de42ee74f3cf47213bf88b2d413b9233ab5fa84c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../mdibella-dev/wordpress-helper',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+    ),
+);
