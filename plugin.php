ddbcef126afb3f8b46e66ee42a5fce1bc38577ff
Marco Di Bella <m_dibella@icloud.com>
2026-07-29 09:57:51 +0200

    Add rank math filter

--- plugin.php
+++ plugin.php
@@ -34,13 +34,15 @@ define( __NAMESPACE__ . '\PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( __NAMESPACE__ . '\PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
 
+
 /** Include files */
 
 require_once PLUGIN_DIR . 'vendor/autoload.php';
+
 require_once PLUGIN_DIR . 'includes/core/index.php';
 require_once PLUGIN_DIR . 'includes/backend/index.php';
 require_once PLUGIN_DIR . 'includes/shortcodes/index.php';
-
+require_once PLUGIN_DIR . 'includes/third-party/index.php';
 
 
 
