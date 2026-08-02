3395a25ca0e6003a5f560272b82dff80000e97c0
Marco <77158987+mdibella-dev@users.noreply.github.com>
2026-08-02 13:46:42 +0200

    Move files to subfolder 'register'

--- includes/core/index.php
+++ includes/core/index.php
@@ -13,14 +13,14 @@ require_once 'api/api-speakers.php';
 
 
 // Custom Post Types
-require_once 'post-types/post-type-speaker.php';
-require_once 'post-types/post-type-session.php';
-require_once 'post-types/post-type-partner.php';
-require_once 'post-types/post-type-exhibition-space.php';
+require_once 'register/post-types/post-type-speaker.php';
+require_once 'register/post-types/post-type-session.php';
+require_once 'register/post-types/post-type-partner.php';
+require_once 'register/post-types/post-type-exhibition-space.php';
 
 
 // Custom Taxonomies
-require_once 'taxonomies/taxonomy-partnership.php';
-require_once 'taxonomies/taxonomy-event.php';
-require_once 'taxonomies/taxonomy-exhibition-package.php';
-require_once 'taxonomies/taxonomy-location.php';
+require_once 'register/taxonomies/taxonomy-partnership.php';
+require_once 'register/taxonomies/taxonomy-event.php';
+require_once 'register/taxonomies/taxonomy-exhibition-package.php';
+require_once 'register/taxonomies/taxonomy-location.php';
